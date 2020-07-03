62a47a74f0bf838846fb234aea20e4e754508d78
Vladislav <vladislavusachev@yandex.ru>
2020-07-03 13:43:54 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/cpageler93/api-client.git", from: "0.2.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.1")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.2.0")),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0")
     ],
     targets: [
